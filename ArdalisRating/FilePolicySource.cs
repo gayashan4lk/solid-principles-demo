c1e178b42baea49e95c16e07f481058a4685de1e
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-11-27 15:04:57 +0530

    refactor code applying SRP

---
+++ ArdalisRating/FilePolicySource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArdalisRating
+{
+    public class FilePolicySource
+    {
+        public string GetDataFromJsonSource()
+        {
+            return File.ReadAllText("policy.json");
+        }
+    }
+}
---
+++ ArdalisRating/JsonPolicySerializer.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArdalisRating
+{
+    public class JsonPolicySerializer
+    {
+        public Policy GetPolicyFromJsonString(string jsonString)
+        {
+            return JsonConvert.DeserializeObject<Policy>(jsonString, new StringEnumConverter());
+        }
+    }
+}
